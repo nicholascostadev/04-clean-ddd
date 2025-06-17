90d2020d0983f425ff8a1c5a10a76557ed152813
Nicholas Costa <nicholascostadev@gmail.com>
2025-06-16 22:26:49 -0300

    feat: add fetch questin comments use case

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -0,0 +1,62 @@
+import { makeQuestion } from "test/factories/make-question";
+import { makeQuestionComment } from "test/factories/make-question-comment";
+import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
+import { FetchQuestionCommentsUseCase } from "./fetch-question-comments";
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: FetchQuestionCommentsUseCase;
+
+describe("Fetch Question Comments", () => {
+	beforeEach(() => {
+		inMemoryQuestionCommentsRepository =
+			new InMemoryQuestionCommentsRepository();
+		sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository);
+	});
+
+	it("should be able to fetch question comments", async () => {
+		const question = makeQuestion();
+		await inMemoryQuestionCommentsRepository.create(
+			makeQuestionComment({
+				questionId: question.id,
+				createdAt: new Date(2022, 0, 20),
+			}),
+		);
+		await inMemoryQuestionCommentsRepository.create(
+			makeQuestionComment({
+				questionId: question.id,
+				createdAt: new Date(2022, 0, 18),
+			}),
+		);
+		await inMemoryQuestionCommentsRepository.create(
+			makeQuestionComment({
+				questionId: question.id,
+				createdAt: new Date(2022, 0, 23),
+			}),
+		);
+
+		const { questionComments } = await sut.execute({
+			questionId: question.id.toString(),
+			page: 1,
+		});
+
+		expect(questionComments).toHaveLength(3);
+	});
+
+	it("should be able to fetch paginated question comments", async () => {
+		const question = makeQuestion();
+		for (let i = 0; i < 22; i++) {
+			await inMemoryQuestionCommentsRepository.create(
+				makeQuestionComment({
+					questionId: question.id,
+				}),
+			);
+		}
+
+		const { questionComments } = await sut.execute({
+			questionId: question.id.toString(),
+			page: 2,
+		});
+
+		expect(questionComments).toHaveLength(2);
+	});
+});
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from "@/core/repositories/pagination-params";
 import type { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import type { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
@@ -16,6 +17,14 @@ export class InMemoryQuestionCommentsRepository
 		);
 
 		return questionComment ?? null;
+	}
+
+	async findManyByQuestionId(questionId: string, params: PaginationParams) {
+		const questionComments = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.slice((params.page - 1) * 20, params.page * 20);
+
+		return questionComments;
 	}
 
 	async delete(questionComment: QuestionComment) {
