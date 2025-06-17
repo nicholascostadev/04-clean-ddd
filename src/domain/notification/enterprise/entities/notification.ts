42288785708baf0ee745d2458d92e9ca05063f54
Nicholas Costa <nicholascostadev@gmail.com>
2025-06-17 19:43:45 -0300

    feat: add notification domain and entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,47 @@
+import { Entity } from "@/core/entities/entity";
+import type { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import type { Optional } from "@/core/types/optional";
+
+interface NotificationProps {
+	title: string;
+	content: string;
+	recipientId: UniqueEntityId;
+	createdAt: Date;
+	readAt?: Date;
+}
+
+type CreateNotificationProps = Optional<NotificationProps, "createdAt">;
+
+export class Notification extends Entity<NotificationProps> {
+	static create(props: CreateNotificationProps, id?: UniqueEntityId) {
+		const notification = new Notification(
+			{
+				...props,
+				createdAt: props.createdAt ?? new Date(),
+			},
+			id,
+		);
+
+		return notification;
+	}
+
+	get recipientId() {
+		return this.props.recipientId;
+	}
+
+	get title() {
+		return this.props.title;
+	}
+
+	get content() {
+		return this.props.content;
+	}
+
+	get createdAt() {
+		return this.props.createdAt;
+	}
+
+	get readAt() {
+		return this.props.readAt;
+	}
+}
